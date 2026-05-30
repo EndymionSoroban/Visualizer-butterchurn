d5b15d2bf5b55357fab10f2ca7da2dc3d6d55c44
SadB <piotr.m.nalewajko@gmail.com>
2026-05-30 11:32:02 +0200

    Add custom _Psychodelic_organic preset

--- src/visualizerManager.ts
+++ src/visualizerManager.ts
@@ -1,5 +1,6 @@
 import butterchurn from 'butterchurn';
 import butterchurnPresets from 'butterchurn-presets';
+import customPsychedelicOrganic from './custom-presets/_Psychodelic_organic.json';
 
 export interface VisualizerPreset {
   name: string;
@@ -38,6 +39,7 @@ class VisualizerManager {
 
     // Load presets
     this.presets = butterchurnPresets.getPresets();
+    this.presets['_Psychodelic_organic'] = customPsychedelicOrganic;
     const presetNames = Object.keys(this.presets);
     if (presetNames.length > 0) {
       // Pick a random default preset
